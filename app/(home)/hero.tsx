750184fb9754c63072102a7c08a98613c7a1d25f
Astra <abhaypratapsinghwd@gmail.com>
2025-07-29 14:18:12 +0530

    Responsive Hero Section

--- app/(home)/hero.tsx
+++ app/(home)/hero.tsx
@@ -20,7 +20,7 @@ function Hero() {
       variants={containerVariant}
       initial="hidden"
       whileInView="visible"
-      className="relative flex flex-col gap-4 items-center text-center min-h-screen justify-center px-4 overflow-hidden"
+      className="relative flex flex-col gap-8 items-center text-center min-h-screen justify-center px-4 overflow-hidden"
     >
       <motion.div variants={scaleInVariant}>
         <a href={siteConfig.links.twitter} target="_blank">
@@ -52,24 +52,24 @@ function Hero() {
 
       <div className="flex flex-col sm:flex-row gap-4 sm:mt-4 items-center ">
         <Link href="/sign-up">
-          <motion.div variants={scaleInVariant} className="p-1">
+          <motion.div variants={scaleInVariant} className="w-full sm:w-fit">
             <Button
               size="lg"
-              className="group rounded relative flex items-center gap-1 text-md backdrop-blur-md  "
+              className="group rounded relative flex items-center gap-1 text-md backdrop-blur-md w-full "
             >
               Get Started For Free
               <ArrowRight className="ml-2 h-4 w-4 transition-all duration-200 group-hover:translate-x-1" />
             </Button>
           </motion.div>
         </Link>
-        <motion.div variants={scaleInVariant}>
+        <motion.div variants={scaleInVariant} className="w-full sm:w-fit">
           <a href={siteConfig.links.githubRepo} target="_blank">
-            <div className="p-[2px] relative z-[2] overflow-hidden rounded">
+            <div className="p-[2px] relative z-[2] overflow-hidden rounded w-full">
               <MovingBorder />
               <Button
                 variant={"outline"}
                 size="lg"
-                className="flex items-center text-md justify-center gap-2 rounded font-normal z-3 relative backdrop-blur-lg"
+                className="flex items-center text-md justify-center gap-2 rounded font-normal z-3 relative backdrop-blur-lg w-full"
               >
                 <FaGithub className=" h-4 w-4" />
                 View on GitHub
